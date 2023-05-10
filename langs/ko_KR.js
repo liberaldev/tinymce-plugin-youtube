27c394d83757533fd8a7e07920324fc0a330784e
liberaldev <ssharp0614@gmail.com>
2023-05-10 12:49:01 +0900

    add korean support

---
+++ langs/ko_KR.js
@@ -0,0 +1,11 @@
+tinymce.addI18n('en', {
+    'YouTube Tooltip'   : "유튜브",
+    'YouTube Title'     : "유튜브 동영상 삽임",
+    'Youtube URL'       : '링크 공유',
+    'Youtube ID'        : 'http://youtu.be/xxxxxxxx or http://www.youtube.com/watch?v=xxxxxxxx',
+    'width'             : '폭',
+    'height'            : '높이',
+    'autoplay'          : '자동 재생',
+    'cancel'            : '취소',
+    'Insert'            : '삽입'
+});
